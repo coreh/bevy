47f679ac63eb6cf4bd36186ca6e1b1212e5bf51c
Marco Buono <thecoreh@gmail.com>
2024-01-14 22:12:21 -0300

    Split BRP handling code in multiple functions

--- crates/bevy_remote/src/lib.rs
+++ crates/bevy_remote/src/lib.rs
@@ -29,7 +29,7 @@ impl Plugin for RemotePlugin {
         let mut order = app.world.resource_mut::<MainScheduleOrder>();
         order.insert_after(First, ProcessBrp);
 
-        app.add_systems(ProcessBrp, process_brp_requests);
+        app.add_systems(ProcessBrp, process_brp_sessions);
 
         app.insert_resource(BrpSessions::default());
 
@@ -98,213 +98,190 @@ impl BrpSessions {
 #[derive(Debug, Hash, PartialEq, Eq, Clone, ScheduleLabel)]
 pub struct ProcessBrp;
 
-fn process_brp_requests(world: &mut World) {
+fn process_brp_sessions(world: &mut World) {
     let sessions = (*world.resource::<BrpSessions>()).clone();
+    for session in sessions.0.iter() {
+        process_brp_session(world, session);
+    }
+}
 
+fn process_brp_session(world: &mut World, session: &BrpSession) {
+    loop {
+        let request = match session.request_receiver.try_recv() {
+            Ok(request) => request,
+            Err(err) => match err {
+                crossbeam_channel::TryRecvError::Empty => break, // no more requests for now
+                crossbeam_channel::TryRecvError::Disconnected => {
+                    panic!("BRP request channel disconnected")
+                }
+            },
+        };
+
+        let response = process_brp_request(world, &session, &request);
+
+        match session.response_sender.send(response) {
+            Ok(_) => {}
+            Err(err) => {
+                panic!("BRP response channel disconnected: {:?}", err)
+            }
+        }
+
+        debug!("Received {:?} from session {:?}", request, session.label);
+    }
+}
+
+fn process_brp_request(
+    world: &mut World,
+    session: &BrpSession,
+    request: &BrpRequest,
+) -> BrpResponse {
+    match request.request {
+        BrpRequestContent::Ping => BrpResponse::new(request.id, BrpResponseContent::Ok),
+        BrpRequestContent::Query {
+            ref data,
+            ref filter,
+        } => process_brp_query_request(world, session, request.id, data, filter),
+        _ => BrpResponse::from_error(request.id, BrpError::Unimplemented),
+    }
+}
+
+fn process_brp_query_request(
+    world: &mut World,
+    session: &BrpSession,
+    id: BrpId,
+    data: &BrpQueryData,
+    filter: &BrpQueryFilter,
+) -> BrpResponse {
     let type_registry_arc = (**world.resource::<AppTypeRegistry>()).clone();
 
     let mut mentioned_components = HashSet::<String>::new();
-    let mut component_ids_by_string = HashMap::<String, ComponentId>::new();
-    let mut type_ids_by_string = HashMap::<String, TypeId>::new();
+    let mut component_id_map = HashMap::<String, ComponentId>::new();
+    let mut type_id_map = HashMap::<String, TypeId>::new();
 
-    for session in sessions.0.iter() {
-        'request_loop: loop {
-            let request = match session.request_receiver.try_recv() {
-                Ok(request) => request,
-                Err(err) => match err {
-                    crossbeam_channel::TryRecvError::Empty => break, // no more requests for now
-                    crossbeam_channel::TryRecvError::Disconnected => {
-                        panic!("BRP request channel disconnected")
-                    }
-                },
-            };
+    for component_name in data
+        .components
+        .iter()
+        .chain(data.optional.iter())
+        .chain(data.has.iter())
+        .chain(filter.with.iter())
+        .chain(filter.without.iter())
+    {
+        mentioned_components.insert(component_name.0.clone());
+    }
 
-            debug!("Received {:?} from session {:?}", request, session.label);
-
-            match request.request {
-                BrpRequestContent::Ping => {
-                    session
-                        .response_sender
-                        .send(BrpResponse::new(request.id, BrpResponseContent::Ok))
-                        .unwrap();
-                }
-                BrpRequestContent::Query { data, filter } => {
-                    for component_name in data
-                        .components
-                        .iter()
-                        .chain(data.optional.iter())
-                        .chain(data.has.iter())
-                        .chain(filter.with.iter())
-                        .chain(filter.without.iter())
-                    {
-                        mentioned_components.insert(component_name.0.clone());
-                    }
-
-                    for component in world.components().iter() {
-                        let name = component.name();
-                        if mentioned_components.contains(name) {
-                            component_ids_by_string.insert(name.to_string(), component.id());
-                            if let Some(type_id) = component.type_id() {
-                                type_ids_by_string.insert(name.to_string(), type_id);
-                            }
-                        }
-                    }
-
-                    let mut builder = QueryBuilder::<FilteredEntityRef>::new(world);
-
-                    for component_name in &data.components {
-                        let Some(component_id) = component_ids_by_string.get(&component_name.0)
-                        else {
-                            session
-                                .response_sender
-                                .send(BrpResponse::from_error(
-                                    request.id,
-                                    BrpError::ComponentNotFound(component_name.0.clone()),
-                                ))
-                                .unwrap();
-                            break 'request_loop;
-                        };
-
-                        builder.ref_id(*component_id);
-                    }
-
-                    for component_name in &data.optional {
-                        let Some(component_id) = component_ids_by_string.get(&component_name.0)
-                        else {
-                            session
-                                .response_sender
-                                .send(BrpResponse::from_error(
-                                    request.id,
-                                    BrpError::ComponentNotFound(component_name.0.clone()),
-                                ))
-                                .unwrap();
-                            break 'request_loop;
-                        };
-                        builder.optional(|query| {
-                            query.ref_id(*component_id);
-                        });
-                    }
-
-                    for component_name in &data.has {
-                        let Some(component_id) = component_ids_by_string.get(&component_name.0)
-                        else {
-                            session
-                                .response_sender
-                                .send(BrpResponse::from_error(
-                                    request.id,
-                                    BrpError::ComponentNotFound(component_name.0.clone()),
-                                ))
-                                .unwrap();
-                            break 'request_loop;
-                        };
-
-                        builder.optional(|query| {
-                            query.ref_id(*component_id);
-                        });
-                    }
-
-                    for component_name in &filter.with {
-                        let Some(component_id) = component_ids_by_string.get(&component_name.0)
-                        else {
-                            session
-                                .response_sender
-                                .send(BrpResponse::from_error(
-                                    request.id,
-                                    BrpError::ComponentNotFound(component_name.0.clone()),
-                                ))
-                                .unwrap();
-                            break 'request_loop;
-                        };
-
-                        builder.with_id(*component_id);
-                    }
-
-                    for component_name in &filter.without {
-                        let Some(component_id) = component_ids_by_string.get(&component_name.0)
-                        else {
-                            session
-                                .response_sender
-                                .send(BrpResponse::from_error(
-                                    request.id,
-                                    BrpError::ComponentNotFound(component_name.0.clone()),
-                                ))
-                                .unwrap();
-                            break 'request_loop;
-                        };
-
-                        builder.without_id(*component_id);
-                    }
-
-                    let mut query = builder.build();
-
-                    let mut results = BrpQueryResults::default();
-
-                    for entity in query.iter(world) {
-                        let mut result = BrpQueryResult {
-                            entity: BrpEntity(entity.id()),
-                            components: HashMap::new(),
-                            optional: HashMap::new(),
-                            has: HashMap::new(),
-                        };
-
-                        for component_name in &data.components {
-                            let component_id = component_ids_by_string[&component_name.0];
-                            let Some(type_id) = type_ids_by_string.get(&component_name.0) else {
-                                session
-                                    .response_sender
-                                    .send(BrpResponse::from_error(
-                                        request.id,
-                                        BrpError::ComponentNotReflectable(component_name.0.clone()),
-                                    ))
-                                    .unwrap();
-                                break 'request_loop;
-                            };
-
-                            let type_registry = type_registry_arc.read();
-
-                            let type_registration = type_registry.get(*type_id).unwrap();
-
-                            let reflect_from_ptr =
-                                type_registration.data::<ReflectFromPtr>().unwrap();
-
-                            let component_ptr = entity.get_by_id(component_id).unwrap();
-
-                            let reflect = unsafe { reflect_from_ptr.as_reflect(component_ptr) };
-
-                            let serializer = ReflectSerializer::new(reflect, &type_registry);
-
-                            let output = match session.component_format {
-                                BrpComponentFormat::Ron => {
-                                    BrpComponent::Ron(ron::ser::to_string(&serializer).unwrap())
-                                }
-                                BrpComponentFormat::Json => BrpComponent::Json(
-                                    serde_json::ser::to_string(&serializer).unwrap(),
-                                ),
-                            };
-
-                            result.components.insert(component_name.clone(), output);
-                        }
-
-                        results.push(result);
-                    }
-
-                    match session.response_sender.send(BrpResponse::new(
-                        request.id,
-                        BrpResponseContent::Query { entities: results },
-                    )) {
-                        Ok(_) => {}
-                        Err(err) => {
-                            panic!("BRP response channel disconnected: {:?}", err)
-                        }
-                    }
-                }
-                _ => {
-                    session
-                        .response_sender
-                        .send(BrpResponse::from_error(request.id, BrpError::Unimplemented))
-                        .unwrap();
-                }
+    for component in world.components().iter() {
+        let name = component.name();
+        if mentioned_components.contains(name) {
+            component_id_map.insert(name.to_string(), component.id());
+            if let Some(type_id) = component.type_id() {
+                type_id_map.insert(name.to_string(), type_id);
             }
         }
     }
+
+    let mut builder = QueryBuilder::<FilteredEntityRef>::new(world);
+
+    for component_name in &data.components {
+        let Some(component_id) = component_id_map.get(&component_name.0) else {
+            return BrpResponse::from_error(
+                id,
+                BrpError::ComponentNotFound(component_name.0.clone()),
+            );
+        };
+
+        builder.ref_id(*component_id);
+    }
+
+    for component_name in &data.optional {
+        let Some(component_id) = component_id_map.get(&component_name.0) else {
+            return BrpResponse::from_error(
+                id,
+                BrpError::ComponentNotFound(component_name.0.clone()),
+            );
+        };
+        builder.optional(|query| {
+            query.ref_id(*component_id);
+        });
+    }
+
+    for component_name in &data.has {
+        let Some(component_id) = component_id_map.get(&component_name.0) else {
+            return BrpResponse::from_error(
+                id,
+                BrpError::ComponentNotFound(component_name.0.clone()),
+            );
+        };
+
+        builder.optional(|query| {
+            query.ref_id(*component_id);
+        });
+    }
+
+    for component_name in &filter.with {
+        let Some(component_id) = component_id_map.get(&component_name.0) else {
+            return BrpResponse::from_error(
+                id,
+                BrpError::ComponentNotFound(component_name.0.clone()),
+            );
+        };
+
+        builder.with_id(*component_id);
+    }
+
+    for component_name in &filter.without {
+        let Some(component_id) = component_id_map.get(&component_name.0) else {
+            return BrpResponse::from_error(
+                id,
+                BrpError::ComponentNotFound(component_name.0.clone()),
+            );
+        };
+
+        builder.without_id(*component_id);
+    }
+
+    let mut query = builder.build();
+
+    let mut results = BrpQueryResults::default();
+
+    for entity in query.iter(world) {
+        let mut result = BrpQueryResult {
+            entity: BrpEntity(entity.id()),
+            components: HashMap::new(),
+            optional: HashMap::new(),
+            has: HashMap::new(),
+        };
+
+        for component_name in &data.components {
+            let component_id = component_id_map[&component_name.0];
+            let Some(type_id) = type_id_map.get(&component_name.0) else {
+                return BrpResponse::from_error(
+                    id,
+                    BrpError::ComponentNotReflectable(component_name.0.clone()),
+                );
+            };
+
+            let type_registry = type_registry_arc.read();
+            let type_registration = type_registry.get(*type_id).unwrap();
+            let reflect_from_ptr = type_registration.data::<ReflectFromPtr>().unwrap();
+            let component_ptr = entity.get_by_id(component_id).unwrap();
+            let reflect = unsafe { reflect_from_ptr.as_reflect(component_ptr) };
+
+            let serializer = ReflectSerializer::new(reflect, &type_registry);
+
+            let output = match session.component_format {
+                BrpComponentFormat::Ron => {
+                    BrpComponent::Ron(ron::ser::to_string(&serializer).unwrap())
+                }
+                BrpComponentFormat::Json => {
+                    BrpComponent::Json(serde_json::ser::to_string(&serializer).unwrap())
+                }
+            };
+
+            result.components.insert(component_name.clone(), output);
+        }
+
+        results.push(result);
+    }
+
+    BrpResponse::new(id, BrpResponseContent::Query { entities: results })
 }
